e23b206b58a72bb2736af7971bcfb097aab85ec7
Elsa Rosenblad <elsa.rosenblads@gmail.com>
2025-04-10 10:33:23 +0200

    MUMmer new script again

--- code/06_mummer_canu.sh
+++ code/06_mummer_canu.sh
@@ -2,32 +2,31 @@
 #SBATCH -A uppmax2025-3-3
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 01:00:00
 #SBATCH -J mummer_canu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elsa.rosenblad.9391@student.uu.se
 #SBATCH --output=%x.%j.out
 
-# Load MUMmer
+# Load necessary modules
 module load bioinfo-tools
 module load MUMmer/4.0.0beta2
 
-# Define input files
-REF=./data/reference_genome/E745_reference.fasta.gz
-QUERY=./analyses/01_canu_assembly/genome_assembly.contigs.fasta
-OUTDIR=./analyses/05_mummer_canu
+# Define full absolute paths
+REF="/domus/h1/elro9391/Genome_Analysis_Enterococcus_Faecium/data/reference_genome/E745_reference.fasta"
+QUERY="/domus/h1/elro9391/Genome_Analysis_Enterococcus_Faecium/analyses/01_canu_assembly/genome_assembly.contigs.fasta"
+
+# Create output dir if not exists
+OUTDIR="./analyses/05_mummer_canu"
 mkdir -p $OUTDIR
 cd $OUTDIR
 
-# Align query to reference
-nucmer --prefix=canu_vs_ref $REF $QUERY
+# Run MUMmer
+nucmer --prefix=canu_vs_ref "$REF" "$QUERY"
 
-# Filter for best alignments (1-to-1 mapping)
+# Filter alignments
 delta-filter -1 canu_vs_ref.delta > canu_vs_ref.filtered.delta
 
-# Generate plot
-mummerplot --png --layout --filter \
-  --prefix=canu_vs_ref_plot canu_vs_ref.filtered.delta
-
-
+# Create plot
+mummerplot --png --fat --layout -p canu_vs_ref canu_vs_ref.filtered.delta
